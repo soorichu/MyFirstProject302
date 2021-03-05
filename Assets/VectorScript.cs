c95e572c412a4dd4e0d5a2d94f00defd7ec3a0ae
soorichu <soorichu@gmail.com>
2021-03-05 12:03:10 +0900

    Initial commit

---
+++ Assets/VectorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorScript : MonoBehaviour
+{
+    Vector3 moveVelocity = new Vector3(0.3f, 0.0f, 0.3f);
+    void Start()
+    {
+        Vector3 playerPos = new Vector3(-3.0f, 0.0f, -4.0f);
+        transform.position = playerPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveVelocity * Time.deltaTime);
+    }
+}
